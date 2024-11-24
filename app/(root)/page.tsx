31b895082709a595a0271ad44fc08e46efe15526
Emanuele-web04 <emanueledipietro612@gmail.com>
2024-11-24 20:37:17 +0100

    jsjj

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
 import Header from "@/components/Header";
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
